dc2ade127cac080873792da4b93530b00dd030a2
HK-SHAO <HK-SHAO@outlook.com>
2026-09-11 04:09:38 +0800

    version

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,8 @@ export function App() {
             演示
           </button>
           <span className="credit">
-            {CREDIT_NAME} · created by{" "}
+            纯客户端运行，不上传任何数据<br/>
+            {CREDIT_NAME} created by{" "}
             <a href={CREDIT_AUTHOR.url} target="_blank" rel="noreferrer">
               {CREDIT_AUTHOR.name}
             </a>
